37805232407e62d087d3bf0f8178ca23d17e41cf
kjh <lykim0418@naver.com>
2023-02-19 22:12:37 +0900

    #3 - 책 풀이 추가
    `BufferedReader` : 받는 값의 개수가 많은 경우에는 Scanner 보단 BufferedReader가 더 성능이 좋다.
    
    `StringTokenizer` : 값이 많을 경우 값을 토큰으로 분리해서 사용하는 것이 좋다.

--- src/main/java/com/baekjoon/javaTest/chapter1/example3.java
+++ src/main/java/com/baekjoon/javaTest/chapter1/example3.java
@@ -1,13 +1,15 @@
 package com.baekjoon.javaTest.chapter1;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class example3 {
     /*
     * 문제
     * 수 N개가 주어졌을 때 i번째 수에서 j번째 수까지의 합을 구하는 프로그램
-    * */
+    *
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
@@ -26,6 +28,30 @@ public class example3 {
             //구간의 합 시작(start), 구간의 합 끝(end)
             int start = scanner.nextInt();
             int end = scanner.nextInt();
+            System.out.println(arr[end] - arr[start-1]);
+        }
+    }
+    */
+
+
+    /** 책 풀이 */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int number = Integer.parseInt(stringTokenizer.nextToken());
+        int count = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] arr = new long[number+1];
+
+        for(int i=1; i<=number; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for(int j=0; j<count; j++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int start = Integer.parseInt(stringTokenizer.nextToken());
+            int end = Integer.parseInt(stringTokenizer.nextToken());
             System.out.println(arr[end] - arr[start-1]);
         }
     }
